a2f6508aa613f5321e79e92d61d821b993468029
Taylor Abbott <abbotttaa@gmail.com>
2017-10-06 11:16:39 -0500

    First commit. all classes are shelled out

---
+++ Bazinglebazgo/GameMechanics.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazinglebazgo
+{
+    class GameMechanics
+    {
+        //member variables
+        MatchUpChart comparisons = new MatchUpChart();
+        //constructor
+        public GameMechanics()
+        {
+            
+        }
+        //member methods
+        public string choseYourChallenger()
+        {
+            Console.WriteLine("Are you playing with another person, or agaisnt the computer? Please enter \"Computer\" or \"person\"");
+        }
+
+        public void round()
+        {
+            player1.MakeChoice();
+            player2.MakeChoice();
+            comparisons.MatchUpWinner();
+        }
+    }
+}
---
+++ Bazinglebazgo/MatchUpChart.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazinglebazgo
+{
+    class MatchUpChart
+    {
+        //member variables
+
+        //constructor
+        //member methods
+        public void MatchUpWinner()
+        {
+            switch ()
+            {
+                case : //r vs p
+                    Console.WriteLine("Player 2 wins!")
+                    break;
+
+                case : //r vs s
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //r vs l
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //r vs sp
+                    Console.WriteLine("Player 2 wins!")
+                    break;
+
+                case : //p vs r
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //p vs s
+                    Console.WriteLine("Player 2 wins!")
+                    break;
+
+                case : //p vs l
+                    Console.WriteLine("Player 2 wins!")
+                    break;
+
+                case : //p vs sp
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //s vs r
+                    Console.WriteLine("Player 2 wins!")
+                    break;
+
+                case : //s vs p
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //s vs l
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //s vs sp
+                    Console.WriteLine("Player 2 wins!")
+                    break;
+
+                case : //l vs r
+                    Console.WriteLine("Player 2 wins!")
+                    break;
+
+                case : //l vs p
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //l vs s
+                    Console.WriteLine("Player 2 wins!")
+                    break;
+
+                case : //l vs sp
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //sp vs r
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //sp vs p
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //sp vs s
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+
+                case : //sp vs l
+                    Console.WriteLine("Player 1 wins!")
+                    break;
+            }
+    }
+}
